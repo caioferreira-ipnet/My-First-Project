b6d1c4e94dace015df45698fc8fc09bc74e1376b
Caio Ferreria <caio.ferreira@ipnet.cloud>
2026-09-18 08:45:33 -0300

    tarefa123

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
 msg = "Hello world";
 print(msg);
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("Caio Gustavo"));
+
+def nameCompleted(name, surname):
+    return f"Hello, {name} {surname}!";
+
+print(nameCompleted("Caio", "Gustavo"));
